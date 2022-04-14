c8fe9b47f4053780c014486ba724e7066c528269
Otar Qartlelishvili <qartlelishvilio@gmail.com>
2022-04-14 18:48:37 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [searchField, setSearchField] = useState('');
+    const [title, setTitle] = useState('');
+    const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setMonsters(users))
+    }, []);
+
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter(monster => {
+            return monster.name.toLowerCase().includes(searchField);
+        })
+        setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField])
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value.trim().toLowerCase());
+    }
+
+    const onTitleChange = (event) => {
+        setTitle(event.target.value.trim().toLowerCase());
+    }
+
+    return (
+        <div className="App">
+            <h1 className='app-title'>Monsters Rolodex</h1>
+            <SearchBox
+                onChangeHandler={onSearchChange}
+                placeholder='search monsters'
+                className='search-box'
+            />
+            <br/>
+            {/*<SearchBox*/}
+            {/*    onChangeHandler={onTitleChange}*/}
+            {/*    placeholder='search title'*/}
+            {/*    className='title-search-box'*/}
+            {/*/>*/}
+            <CardList monsters={filteredMonsters}/>
+        </div>
+    );
 }
 
 export default App;
